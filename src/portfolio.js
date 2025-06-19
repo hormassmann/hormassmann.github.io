b6df59ed6d457fabdda113e71585179ce486d8e0
ENDUVY <160536960+ENDUVY@users.noreply.github.com>
2025-06-19 15:43:11 +0200

    personalized

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,21 +7,21 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Horm's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Horm Assmann Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "https://hormassmann.github.io/",
   },
 };
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Horm Assmann",
+  logo_name: "HormAssmann",
+  nickname: "Horm",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
